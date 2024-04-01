5a07ec3df9cfa0a7e885317c7a0b4d7e7728f47b
FelipeTakahashi <felipejun@outlook.com>
2024-04-01 10:56:11 -0400

    insere_constroi

--- src/hash.c
+++ src/hash.c
@@ -6,15 +6,6 @@
 #include "../include/libfacom.h"
 #define SEED    0x12345678
 
-// typedef struct {
-//      uintptr_t * table;
-//      int size;
-//      int max;
-//      uintptr_t deleted;
-//      char * (*get_key)(void *);
-// }thash;
-
-
 uint32_t hashf(const char* str, uint32_t h){
     /* One-byte-at-a-time Murmur hash 
     Source: https://github.com/aappleby/smhasher/blob/master/src/Hashes.cpp */
@@ -28,7 +19,21 @@ uint32_t hashf(const char* str, uint32_t h){
 
 
 int hash_insere(thash * h, void * bucket){
-   //int bucket = hashf();
+    uint32_t hash = hashf(h->get_key(bucket), SEED);
+    int pos = hash % (h->max);
+    int tableSize = h->table + h->size;
+    
+    if(h->max == h->size+1) { 
+        free(bucket);
+        return EXIT_FAILURE;
+    }else {
+        while(h->table[pos] != NULL) {
+            if(h->table[pos] == h->deleted) break;
+            pos = (pos+1)%h->max;
+        }
+        h->table[pos] = (uintptr_t)bucket;
+        h->size += 1;
+    }
 
     return EXIT_SUCCESS;
 
@@ -37,7 +42,16 @@ int hash_insere(thash * h, void * bucket){
 
 
 int hash_constroi(thash * h,int nbuckets, char * (*get_key)(void *) ){
-	//int bucket = hashf(*(*get_key));
+    h->table = calloc(nbuckets+1, sizeof(void*));
+    if(h->table == NULL) {
+        return EXIT_FAILURE;
+    }
+
+    h->max = nbuckets + 1;
+    h->size = 0;
+    h->deleted = (uintptr_t)&(h->size); 
+    h->get_key = get_key;
+
     return EXIT_SUCCESS;
 
 }
